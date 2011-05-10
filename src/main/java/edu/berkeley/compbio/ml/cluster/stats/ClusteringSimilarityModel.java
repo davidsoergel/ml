6447b9b08ebf49234732a9eca24f88bc6eb98399
David Soergel <dev@davidsoergel.com>
2011-05-10 02:11:29 +0000

    Performance, bugfixes

--- src/main/java/edu/berkeley/compbio/ml/cluster/stats/ClusteringSimilarityModel.java
+++ src/main/java/edu/berkeley/compbio/ml/cluster/stats/ClusteringSimilarityModel.java
@@ -40,174 +40,168 @@ public class ClusteringSimilarityModel<T extends Clusterable<T> & Comparable<T>>
 	public final double pairwiseSameClusterSpecificity;
 
 	public ClusteringSimilarityModel(final ClusterList<T> theClusterList, final ClusterList<T> referenceClusterList,
-	                                 int totalSamples) //, final Comparator<T> sampleComparator)
-	{
-
-	int totalSamplePairs = (totalSamples * (totalSamples - 1)) / 2;
-
-	// Sensitivity/specificity of detecting clusters perfectly between scrambled and reference
-
-	List<? extends Cluster<? extends T>> referenceClusters = referenceClusterList.getClusters();
-	List<? extends Cluster<? extends T>> clusters = theClusterList.getClusters();
-
-	Set<BatchCluster<T, ?>> castReferenceClusters = new HashSet<BatchCluster<T, ?>>();
-	for (Cluster<? extends T> referenceCluster : referenceClusters)
+	                                 int totalSamples)
 		{
-		castReferenceClusters.add((BatchCluster<T, ?>) referenceCluster);
-		}
 
-	Set<BatchCluster<T, ?>> castClusters = new HashSet<BatchCluster<T, ?>>();
-	for (Cluster<? extends T> cluster : clusters)
-		{
-		castClusters.add((BatchCluster<T, ?>) cluster);
-		}
+		int totalSamplePairs = (totalSamples * (totalSamples - 1)) / 2;
 
-	Set<BatchCluster<T, ?>> identicalClusters =
-			DSCollectionUtils.intersectionFast(castReferenceClusters, castClusters, new IdenticalClusterComparator());
+		// Sensitivity/specificity of detecting clusters perfectly between scrambled and reference
 
+		List<? extends Cluster<? extends T>> referenceClusters = referenceClusterList.getClusters();
+		List<? extends Cluster<? extends T>> clusters = theClusterList.getClusters();
 
-	proportionOfReferenceClustersIdentical = (double) identicalClusters.size() / (double) castReferenceClusters.size();
-	proportionOfPredictedClustersIdentical = (double) identicalClusters.size() / (double) castClusters.size();
-
-
-	// look for clusters that can be unambiguously mapped to one another
-
-	Set<BatchCluster<T, ?>> mappableReferenceClusters = DSCollectionUtils
-			.intersectionExhaustive(castReferenceClusters, castClusters, new MappableClusterComparator(0.5));
-	Set<BatchCluster<T, ?>> mappablePredictedClusters = DSCollectionUtils
-			.intersectionExhaustive(castClusters, castReferenceClusters, new MappableClusterComparator(0.5));
-
-	// obviously the number of mappable clusters must be the same, since the mapping is 1-1, though the actual clusters are different
-	// oops this is no longer true since the "mappable" thing is now asymmetric
-	// assert mappablePredictedClusters.size() == mappableReferenceClusters.size();
-
-	proportionOfReferenceClustersMappable =
-			(double) mappableReferenceClusters.size() / (double) castReferenceClusters.size();
-	proportionOfPredictedClustersMappable = (double) mappableReferenceClusters.size() / (double) castClusters.size();
-
-
-	// Sensitivity/specificity of pairwise taxon OTU co-occurrence.  I.e., being in the same cluster in the reference clustering is the goal; how well is that predicted?
-
-	SortedSet<UnorderedPair<T>> referenceSameClusterSamplePairs = new TreeSet<UnorderedPair<T>>();
-	for (BatchCluster<T, ?> cluster : castReferenceClusters)
-		{
-		SortedSet<T> points = new TreeSet<T>(cluster.getPoints());
-		for (T point : points)
+		Set<BatchCluster<T, ?>> castReferenceClusters = new HashSet<BatchCluster<T, ?>>();
+		for (Cluster<? extends T> referenceCluster : referenceClusters)
 			{
-			for (T t : points)
+			castReferenceClusters.add((BatchCluster<T, ?>) referenceCluster);
+			}
+
+		Set<BatchCluster<T, ?>> castClusters = new HashSet<BatchCluster<T, ?>>();
+		for (Cluster<? extends T> cluster : clusters)
+			{
+			castClusters.add((BatchCluster<T, ?>) cluster);
+			}
+
+		Set<BatchCluster<T, ?>> identicalClusters = DSCollectionUtils
+				.intersectionFast(castReferenceClusters, castClusters, new IdenticalClusterComparator());
+
+
+		proportionOfReferenceClustersIdentical =
+				(double) identicalClusters.size() / (double) castReferenceClusters.size();
+		proportionOfPredictedClustersIdentical = (double) identicalClusters.size() / (double) castClusters.size();
+
+
+		// look for clusters that can be unambiguously mapped to one another
+
+		Set<BatchCluster<T, ?>> mappableReferenceClusters = DSCollectionUtils
+				.intersectionExhaustive(castReferenceClusters, castClusters, new MappableClusterComparator(0.5));
+		Set<BatchCluster<T, ?>> mappablePredictedClusters = DSCollectionUtils
+				.intersectionExhaustive(castClusters, castReferenceClusters, new MappableClusterComparator(0.5));
+
+		// obviously the number of mappable clusters must be the same, since the mapping is 1-1, though the actual clusters are different
+		// oops this is no longer true since the "mappable" thing is now asymmetric
+		// assert mappablePredictedClusters.size() == mappableReferenceClusters.size();
+
+		proportionOfReferenceClustersMappable =
+				(double) mappableReferenceClusters.size() / (double) castReferenceClusters.size();
+		proportionOfPredictedClustersMappable =
+				(double) mappableReferenceClusters.size() / (double) castClusters.size();
+
+
+		// Sensitivity/specificity of pairwise taxon OTU co-occurrence.  I.e., being in the same cluster in the reference clustering is the goal; how well is that predicted?
+
+		SortedSet<UnorderedPair<T>> referenceSameClusterSamplePairs = new TreeSet<UnorderedPair<T>>();
+		for (BatchCluster<T, ?> cluster : castReferenceClusters)
+			{
+			SortedSet<T> points = cluster.getPoints(); //new TreeSet<T>(cluster.getPoints());
+			for (T point : points)
 				{
-				if (t.compareTo(point) >= 0)
+				for (T t : points.headSet(point))
 					{
-					break;
+					referenceSameClusterSamplePairs.add(new UnorderedPair<T>(point, t));
 					}
-				referenceSameClusterSamplePairs.add(new UnorderedPair<T>(point, t));
 				}
 			}
-		}
 
-	SortedSet<UnorderedPair<T>> predictedSameClusterSamplePairs = new TreeSet<UnorderedPair<T>>();
-	for (BatchCluster<T, ?> cluster : castClusters)
-		{
-		SortedSet<T> points = new TreeSet<T>(cluster.getPoints());
-		for (T point : points)
+		SortedSet<UnorderedPair<T>> predictedSameClusterSamplePairs = new TreeSet<UnorderedPair<T>>();
+		for (BatchCluster<T, ?> cluster : castClusters)
 			{
-			for (T t : points)
+			SortedSet<T> points = cluster.getPoints();  //new TreeSet<T>(cluster.getPoints());
+			for (T point : points)
 				{
-				if (t.compareTo(point) >= 0)
+				for (T t : points.headSet(point))
 					{
-					break;
+					predictedSameClusterSamplePairs.add(new UnorderedPair<T>(point, t));
 					}
-				predictedSameClusterSamplePairs.add(new UnorderedPair<T>(point, t));
 				}
 			}
-		}
 
-	// careful: hashCode/equals/compareTo of Clusterable objects may not work right, since the labelling stuff can't be final
-	/*
-	   int truePositives =
-			   DSCollectionUtils.intersection(referenceSameClusterSamplePairs, predictedSameClusterSamplePairs).size();
+		// careful: hashCode/equals/compareTo of Clusterable objects may not work right, since the labelling stuff can't be final
+		/*
+			   int truePositives =
+					   DSCollectionUtils.intersection(referenceSameClusterSamplePairs, predictedSameClusterSamplePairs).size();
 
-	   int falsePositives =
-			   DSCollectionUtils.subtract(predictedSameClusterSamplePairs, referenceSameClusterSamplePairs).size();
-	   int falseNegatives =
-			   DSCollectionUtils.subtract(referenceSameClusterSamplePairs, predictedSameClusterSamplePairs).size();
-	   */
+			   int falsePositives =
+					   DSCollectionUtils.subtract(predictedSameClusterSamplePairs, referenceSameClusterSamplePairs).size();
+			   int falseNegatives =
+					   DSCollectionUtils.subtract(referenceSameClusterSamplePairs, predictedSameClusterSamplePairs).size();
+			   */
 
-	Set<UnorderedPair<T>> truePositives = DSCollectionUtils
-			.intersectionFastUsingCompare(referenceSameClusterSamplePairs, predictedSameClusterSamplePairs);
-	Set<UnorderedPair<T>> falsePositives = DSCollectionUtils
-			.subtractFastUsingCompare(predictedSameClusterSamplePairs, referenceSameClusterSamplePairs);
-	Set<UnorderedPair<T>> falseNegatives = DSCollectionUtils
-			.subtractFastUsingCompare(referenceSameClusterSamplePairs, predictedSameClusterSamplePairs);
+		Set<UnorderedPair<T>> truePositives = DSCollectionUtils
+				.intersectionFastUsingCompare(referenceSameClusterSamplePairs, predictedSameClusterSamplePairs);
+		Set<UnorderedPair<T>> falsePositives = DSCollectionUtils
+				.subtractFastUsingCompare(predictedSameClusterSamplePairs, referenceSameClusterSamplePairs);
+		Set<UnorderedPair<T>> falseNegatives = DSCollectionUtils
+				.subtractFastUsingCompare(referenceSameClusterSamplePairs, predictedSameClusterSamplePairs);
 
-	int trueNegatives = totalSamplePairs - truePositives.size() - falsePositives.size() - falseNegatives.size();
+		int trueNegatives = totalSamplePairs - truePositives.size() - falsePositives.size() - falseNegatives.size();
 
-	pairwiseSameClusterSensitivity =
-			(double) truePositives.size() / (double) (truePositives.size() + falseNegatives.size());
-	pairwiseSameClusterSpecificity = (double) trueNegatives / (double) (trueNegatives + falsePositives.size());
+		pairwiseSameClusterSensitivity =
+				(double) truePositives.size() / (double) (truePositives.size() + falseNegatives.size());
+		pairwiseSameClusterSpecificity = (double) trueNegatives / (double) (trueNegatives + falsePositives.size());
 
 
-	// % of cluster pairs in identical clusters, out of all the true positive pairs
+		// % of cluster pairs in identical clusters, out of all the true positive pairs
 
-	int samplesInIdenticalClusters = 0;
-	int samplePairsInIdenticalClusters = 0;
-	for (BatchCluster<T, ?> identicalCluster : identicalClusters)
-		{
-		int n = identicalCluster.getN();
-		assert n > 0;
-		samplesInIdenticalClusters += n;
-
-		if (n > 1)
+		int samplesInIdenticalClusters = 0;
+		int samplePairsInIdenticalClusters = 0;
+		for (BatchCluster<T, ?> identicalCluster : identicalClusters)
 			{
-			int pairs = (n * (n - 1)) / 2;
-			samplePairsInIdenticalClusters += pairs;
+			int n = identicalCluster.getN();
+			assert n > 0;
+			samplesInIdenticalClusters += n;
+
+			if (n > 1)
+				{
+				int pairs = (n * (n - 1)) / 2;
+				samplePairsInIdenticalClusters += pairs;
+				}
 			}
-		}
-	proportionOfSamplesInIdenticalClusters = (double) samplesInIdenticalClusters / (double) totalSamples;
-	proportionOfTruePositiveSamplePairsInIdenticalClusters =
-			(double) samplePairsInIdenticalClusters / (double) truePositives.size();
+		proportionOfSamplesInIdenticalClusters = (double) samplesInIdenticalClusters / (double) totalSamples;
+		proportionOfTruePositiveSamplePairsInIdenticalClusters =
+				(double) samplePairsInIdenticalClusters / (double) truePositives.size();
 
 
-	// % of cluster pairs in mappable clusters, out of all the positive pairs
+		// % of cluster pairs in mappable clusters, out of all the positive pairs
 
-	int samplesInMappableReferenceClusters = 0;
-	int samplePairsInMappableReferenceClusters = 0;
-	for (BatchCluster<T, ?> mappableCluster : mappableReferenceClusters)
-		{
-		int n = mappableCluster.getN();
-		assert n > 0;
-		samplesInMappableReferenceClusters += n;
-
-		if (n > 1)
+		int samplesInMappableReferenceClusters = 0;
+		int samplePairsInMappableReferenceClusters = 0;
+		for (BatchCluster<T, ?> mappableCluster : mappableReferenceClusters)
 			{
-			int pairs = (n * (n - 1)) / 2;
-			samplePairsInMappableReferenceClusters += pairs;
+			int n = mappableCluster.getN();
+			assert n > 0;
+			samplesInMappableReferenceClusters += n;
+
+			if (n > 1)
+				{
+				int pairs = (n * (n - 1)) / 2;
+				samplePairsInMappableReferenceClusters += pairs;
+				}
 			}
-		}
-	proportionOfSamplesInMappableReferenceClusters =
-			(double) samplesInMappableReferenceClusters / (double) totalSamples;
-	proportionOfSamplePairsInMappableReferenceClusters =
-			(double) samplePairsInMappableReferenceClusters / (double) referenceSameClusterSamplePairs.size();
+		proportionOfSamplesInMappableReferenceClusters =
+				(double) samplesInMappableReferenceClusters / (double) totalSamples;
+		proportionOfSamplePairsInMappableReferenceClusters =
+				(double) samplePairsInMappableReferenceClusters / (double) referenceSameClusterSamplePairs.size();
 
-	int samplesInMappablePredictedClusters = 0;
-	int samplePairsInMappablePredictedClusters = 0;
-	for (BatchCluster<T, ?> mappableCluster : mappablePredictedClusters)
-		{
-		int n = mappableCluster.getN();
-		assert n > 0;
-		samplesInMappablePredictedClusters += n;
-
-		if (n > 1)
+		int samplesInMappablePredictedClusters = 0;
+		int samplePairsInMappablePredictedClusters = 0;
+		for (BatchCluster<T, ?> mappableCluster : mappablePredictedClusters)
 			{
-			int pairs = (n * (n - 1)) / 2;
-			samplePairsInMappablePredictedClusters += pairs;
+			int n = mappableCluster.getN();
+			assert n > 0;
+			samplesInMappablePredictedClusters += n;
+
+			if (n > 1)
+				{
+				int pairs = (n * (n - 1)) / 2;
+				samplePairsInMappablePredictedClusters += pairs;
+				}
 			}
+		proportionOfSamplesInMappablePredictedClusters =
+				(double) samplesInMappablePredictedClusters / (double) totalSamples;
+		proportionOfSamplePairsInMappablePredictedClusters =
+				(double) samplePairsInMappablePredictedClusters / (double) predictedSameClusterSamplePairs.size();
 		}
-	proportionOfSamplesInMappablePredictedClusters =
-			(double) samplesInMappablePredictedClusters / (double) totalSamples;
-	proportionOfSamplePairsInMappablePredictedClusters =
-			(double) samplePairsInMappablePredictedClusters / (double) predictedSameClusterSamplePairs.size();
-	}
 
 
 /*	@Override
