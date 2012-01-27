3a1a4206b04d8e279307e985da0753d7054dc7fb
David Soergel <dev@davidsoergel.com>
2012-01-27 17:02:30 -0500

    debug

--- src/main/java/edu/berkeley/compbio/ml/cluster/hierarchical/OnlineAgglomerativeClustering.java
+++ src/main/java/edu/berkeley/compbio/ml/cluster/hierarchical/OnlineAgglomerativeClustering.java
@@ -21,11 +21,11 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * Agglomeratively cluster points in an online manner.  Clusters are joined when their distance is within the provided
- * threshold.  During the online process, there may be many active clusters that are not related to each other-- their
- * pairwise distances are known to be greater than the threshold, and there is no root.  At the end of the online
- * process, any remaining active nodes (which are still disconnected because they are separated by more than the
- * threshold) are joined by a batch method.
+ * Agglomeratively cluster points in an online manner.  Clusters are joined when their distance is within the provided threshold.  During the online process,
+ * there may be many active clusters that are
+ * not related to each other-- their pairwise distances are known to be greater than the threshold, and there is no root.  At the end of the online process,
+ * any remaining active nodes (which are still
+ * disconnected because they are separated by more than the threshold) are joined by a batch method.
  *
  * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
  * @version $Id$
@@ -35,71 +35,77 @@ public class OnlineAgglomerativeClustering<T extends Clusterable<T>> extends Onl
 	private static final Logger logger = Logger.getLogger(OnlineAgglomerativeClustering.class);
 	public static final Float LONG_DISTANCE = Float.MAX_VALUE;
 
-	protected final IndexedSymmetric2dBiMapWithDefault<HierarchicalCentroidCluster<T>, Float>
-			theActiveNodeDistanceMatrix =
+	protected final IndexedSymmetric2dBiMapWithDefault<HierarchicalCentroidCluster<T>, Float> theActiveNodeDistanceMatrix =
 			new IndexedSymmetric2dBiMapWithDefault<HierarchicalCentroidCluster<T>, Float>(LONG_DISTANCE);
 	private HierarchicalCentroidCluster<T> theRoot;
-	private final Map<T, HierarchicalCentroidCluster<T>> sampleToLeafClusterMap =
-			new HashMap<T, HierarchicalCentroidCluster<T>>();
+	private final Map<T, HierarchicalCentroidCluster<T>> sampleToLeafClusterMap = new HashMap<T, HierarchicalCentroidCluster<T>>();
 	private final AtomicInteger idCount = new AtomicInteger(0);
 	HierarchicalCentroidCluster<T> saveNode;
 	double threshold;
 	Agglomerator<T> agglomerator;
 
-	public OnlineAgglomerativeClustering(final DissimilarityMeasure<T> dm, final Set<String> potentialTrainingBins,
-	                                     final Map<String, Set<String>> predictLabelSets,
-	                                     final ProhibitionModel<T> prohibitionModel, final Set<String> testLabels,
-	                                     Double threshold, Agglomerator agg)
+	public OnlineAgglomerativeClustering( final DissimilarityMeasure<T> dm, final Set<String> potentialTrainingBins, final Map<String, Set<String>> predictLabelSets,
+	                                      final ProhibitionModel<T> prohibitionModel, final Set<String> testLabels, Double threshold, Agglomerator agg )
 		{
 		super(dm, potentialTrainingBins, predictLabelSets, prohibitionModel, testLabels);
 		this.threshold = threshold;
 		this.agglomerator = agg;
 		}
 
+
+	public int numActiveClusters()
+		{
+		return theActiveNodeDistanceMatrix.numKeys();
+		}
+
+	public int numDistances()
+		{
+
+		return theActiveNodeDistanceMatrix.numPairs();
+		}
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public ClusterMove<T, HierarchicalCentroidCluster<T>> bestClusterMove(final T p) throws NoGoodClusterException
+	public ClusterMove<T, HierarchicalCentroidCluster<T>> bestClusterMove( final T p ) throws NoGoodClusterException {
+	final ClusterMove result = new ClusterMove();
+	result.bestDistance = Double.POSITIVE_INFINITY;
+
+	final HierarchicalCentroidCluster<T> c = sampleToLeafClusterMap.get(p);
+	if (c != null)
 		{
-		final ClusterMove result = new ClusterMove();
-		result.bestDistance = Double.POSITIVE_INFINITY;
-
-		final HierarchicalCentroidCluster<T> c = sampleToLeafClusterMap.get(p);
-		if (c != null)
-			{
-			// this sample was part of the initial clustering, so of course the best cluster is the one representing just the sample itself
-			result.bestCluster = c;
-			result.bestDistance = 0;
-			return result;
-			}
-
-		PointClusterFilter<T> clusterFilter = prohibitionModel == null ? null : prohibitionModel.getFilter(p);
-
-		for (final CentroidCluster<T> theCluster : getClusters())
-			{
-			if (clusterFilter != null && clusterFilter.isProhibited(theCluster))
-				{
-				// ignore this cluster
-				}
-			else
-				{
-				final double distance = measure.distanceFromTo(p, theCluster.getCentroid());
-				if (distance < result.bestDistance)
-					{
-					result.bestCluster = theCluster;
-					result.bestDistance = distance;
-					}
-				}
-			}
-		if (result.bestCluster == null)
-			{
-			throw new NoGoodClusterException("No cluster found for point: " + p);
-			}
+		// this sample was part of the initial clustering, so of course the best cluster is the one representing just the sample itself
+		result.bestCluster = c;
+		result.bestDistance = 0;
 		return result;
 		}
 
-	protected synchronized void trainWithKnownTrainingLabels(final ClusterableIterator<T> samples)
+	PointClusterFilter<T> clusterFilter = prohibitionModel == null ? null : prohibitionModel.getFilter(p);
+
+	for (final CentroidCluster<T> theCluster : getClusters())
+		{
+		if (clusterFilter != null && clusterFilter.isProhibited(theCluster))
+			{
+			// ignore this cluster
+			}
+		else
+			{
+			final double distance = measure.distanceFromTo(p, theCluster.getCentroid());
+			if (distance < result.bestDistance)
+				{
+				result.bestCluster = theCluster;
+				result.bestDistance = distance;
+				}
+			}
+		}
+	if (result.bestCluster == null)
+		{
+		throw new NoGoodClusterException("No cluster found for point: " + p);
+		}
+	return result;
+	}
+
+	protected synchronized void trainWithKnownTrainingLabels( final ClusterableIterator<T> samples )
 		{
 		// initialize with two samples
 		T s1 = samples.next();
@@ -117,10 +123,9 @@ public class OnlineAgglomerativeClustering<T extends Clusterable<T>> extends Onl
 
 		Parallel.forEach(samples, new Function<T, Void>()
 		{
-		public Void apply(final T sample)
+		public Void apply( final T sample )
 			{
-			final HierarchicalCentroidCluster<T> c =
-					new HierarchicalCentroidCluster<T>(idCount.getAndIncrement(), sample);
+			final HierarchicalCentroidCluster<T> c = new HierarchicalCentroidCluster<T>(idCount.getAndIncrement(), sample);
 			c.doneLabelling();
 
 			// these is synchronized on the cluster list
@@ -132,14 +137,12 @@ public class OnlineAgglomerativeClustering<T extends Clusterable<T>> extends Onl
 			// In particular, it includes new leaves from other threads that have not yet appeared in the distance matrix.
 
 			// this is synchronized on the distance matrix, so we probably have to wait for another thread to finish its updates
-			final Collection<HierarchicalCentroidCluster<T>> activeClusters =
-					theActiveNodeDistanceMatrix.getKeys();//getClusters();
+			final Collection<HierarchicalCentroidCluster<T>> activeClusters = theActiveNodeDistanceMatrix.getKeys();//getClusters();
 
 			// then things are purely local for a while
 			final int ahc = c.hashCode();
 			// store up the results in this thread before copying them to the synchronized store at the end
-			final Map<HierarchicalCentroidCluster<T>, Float> distancesToC =
-					new HashMap<HierarchicalCentroidCluster<T>, Float>(getClusters().size());
+			final Map<HierarchicalCentroidCluster<T>, Float> distancesToC = new HashMap<HierarchicalCentroidCluster<T>, Float>(getClusters().size());
 
 
 			for (HierarchicalCentroidCluster<T> b : activeClusters)
@@ -151,8 +154,7 @@ public class OnlineAgglomerativeClustering<T extends Clusterable<T>> extends Onl
 				else */
 				if (ahc <= b.hashCode() && !c.equals(b))
 					{
-					final float d =
-							(float) measure.distanceFromTo(c.getPayload().getCentroid(), b.getPayload().getCentroid());
+					final float d = (float) measure.distanceFromTo(c.getPayload().getCentroid(), b.getPayload().getCentroid());
 
 					// concurrency bottleneck
 					//theActiveNodeDistanceMatrix.put(a, b, d);
@@ -180,8 +182,7 @@ public class OnlineAgglomerativeClustering<T extends Clusterable<T>> extends Onl
 
 				if (activeKeys.size() > 0)
 					{
-					final Set<HierarchicalCentroidCluster<T>> remainingActiveKeys =
-							new HashSet<HierarchicalCentroidCluster<T>>(activeKeys);
+					final Set<HierarchicalCentroidCluster<T>> remainingActiveKeys = new HashSet<HierarchicalCentroidCluster<T>>(activeKeys);
 					distancesToC.keySet().retainAll(remainingActiveKeys);
 					for (Map.Entry<HierarchicalCentroidCluster<T>, Float> entry : distancesToC.entrySet())
 						{
@@ -196,8 +197,7 @@ public class OnlineAgglomerativeClustering<T extends Clusterable<T>> extends Onl
 					// catch up and do them now; too bad about the synchronization
 					for (HierarchicalCentroidCluster<T> b : remainingActiveKeys)
 						{
-						final float d = (float) measure
-								.distanceFromTo(c.getPayload().getCentroid(), b.getPayload().getCentroid());
+						final float d = (float) measure.distanceFromTo(c.getPayload().getCentroid(), b.getPayload().getCentroid());
 						theActiveNodeDistanceMatrix.put(c, b, d);
 						}
 
@@ -212,8 +212,7 @@ public class OnlineAgglomerativeClustering<T extends Clusterable<T>> extends Onl
 						assert theActiveNodeDistanceMatrix.getKeys().contains(b);
 						assert !theActiveNodeDistanceMatrix.getKeys().contains(a);
 						assert !theActiveNodeDistanceMatrix.getKeys().contains(b);
-						final HierarchicalCentroidCluster<T> composite =
-								agglomerator.joinNodes(idCount.getAndIncrement(), a, b, theActiveNodeDistanceMatrix);
+						final HierarchicalCentroidCluster<T> composite = agglomerator.joinNodes(idCount.getAndIncrement(), a, b, theActiveNodeDistanceMatrix);
 						theActiveNodeDistanceMatrix.remove(a);
 						theActiveNodeDistanceMatrix.remove(b);
 						//agglomerator.removeJoinedNodes(a, b, theActiveNodeDistanceMatrix);
@@ -227,14 +226,13 @@ public class OnlineAgglomerativeClustering<T extends Clusterable<T>> extends Onl
 			}
 		}
 
-		);
+		                );
 
 		// note the batch clustering phase it not necessary if we just want to count OTUs, but it allows us to use a consistent DepthFirstIterator to find the OTUs later
 
 		BatchAgglomerativeClusteringMethod<T> batchClustering =
-				new BatchAgglomerativeClusteringMethod<T>(measure, potentialTrainingBins, predictLabelSets,
-				                                          prohibitionModel, testLabels, theClusters, assignments, n,
-				                                          agglomerator, theActiveNodeDistanceMatrix);
+				new BatchAgglomerativeClusteringMethod<T>(measure, potentialTrainingBins, predictLabelSets, prohibitionModel, testLabels, theClusters, assignments, n, agglomerator,
+				                                          theActiveNodeDistanceMatrix);
 
 		batchClustering.train();
 		theRoot = batchClustering.getTree();
